6ac198b0b947fe73a85f29bbaf66fc0734960c71
JulienTouchard <julien.touchard@free.fr>
2024-11-28 10:24:25 +0100

    add materialUI card

--- src/components/CardShop/CardShop.js
+++ src/components/CardShop/CardShop.js
@@ -1,44 +1,44 @@
-import React from 'react';
+
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 import './CardShop.css';
 
 function CardShop(props) {
-  
-  const handleClickCard = (id)=>{
+
+  const handleClickCard = (id) => {
     props.click(id)
   }
   return (
     <>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    <div className='card'>
-      <img src={"./assets/img/" + props.data.imgUrl} alt={props.data.name} />
-      <h2>{props.data.name}</h2>
-      <p className='card-price'>{props.data.price}€</p>
-      <p className='card-description'>{props.data.description}</p>
-      <p className='card-qte'>
-      {props.data.qte > 0 ? props.data.qte + " costume(s) en stock" : "Ce produit n'est plus disponible."}
-      </p>
-      <button className='' onClick={()=>handleClickCard(props.data.id)}>Achat</button>
-    </div>
+      <Card sx={{ width: 345 }}>
+        <CardMedia
+          component="img"
+          alt={props.data.name}
+          height="140"
+          image={"./assets/img/" + props.data.imgUrl}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.data.name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {props.data.description}
+          </Typography>
+          <p className='card-price'>{props.data.price}€</p>
+          <p className='card-qte'>
+            {props.data.qte > 0 ? props.data.qte + " costume(s) en stock" : "Ce produit n'est plus disponible."}
+          </p>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => handleClickCard(props.data.id)}>Achat</Button>
+        </CardActions>
+      </Card>
     </>
   )
 }
