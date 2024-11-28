687b51bccbbd1753f4b030918bb61c8091d32b1a
JulienTouchard <julien.touchard@free.fr>
2024-11-28 16:59:22 +0100

    add name

--- src/components/Panier/Panier.js
+++ src/components/Panier/Panier.js
@@ -5,11 +5,16 @@ import { BoutiqueContext } from '../../contexts/BoutiqueContext'
 function Panier() {
     const boutiqueContext = useContext(BoutiqueContext);
     return (
-        <div style={{backgroundColor:"green"}}>
+        <div style={{ backgroundColor: "green" }}>
             <h4>Mon panier</h4>
             {boutiqueContext.statePanier.map(
-                (element,index) => {
-                    return(<div key={index}>{element}</div>)
+                (element, index) => {
+                    let article = boutiqueContext.dataBoutiqueState[element];
+                    return (
+                        <div key={index}>{
+                            article.name
+                        }</div>
+                    )
                 }
             )
             }
